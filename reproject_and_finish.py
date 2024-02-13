ef8c385e0cc34f3c51f851dbdf256cbe5a12c9bb
damas0 <damasoavalos@gmail.com>
2024-02-12 20:38:44 -0700

    reproject_and_finish done

--- reproject_and_finish.py
+++ reproject_and_finish.py
@@ -18,7 +18,8 @@ from qgis.core import (QgsApplication,
                        QgsProcessingParameterBoolean)
 import processing
 
-class reprojectandfinish(QgsProcessingAlgorithm):
+
+class ReprojectAndFinish(QgsProcessingAlgorithm):
 
     INPUT = 'INPUT'     
     VALUEFIELD = 'VALUEFIELD'
@@ -37,28 +38,27 @@ class reprojectandfinish(QgsProcessingAlgorithm):
         self.addParameter(QgsProcessingParameterVectorLayer(self.INPUT, 'Input layer', types=[QgsProcessing.TypeVectorPolygon]))           
         self.addParameter(QgsProcessingParameterFeatureSink(self.OUTPUT, 'Boundaries', type=QgsProcessing.TypeVectorPolygon))
 
-    def ReprojectToWgs84(self, _inLayer, _context, _feedback):
+    def reproject_to_wgs84(self, _in_layer, _context, _feedback):
         if _feedback.isCanceled():
-               return {} 
+            return {}
                   
         _stringWgs84Crs = CRS.from_user_input('WGS 84').to_string()
-        _parameter = {'INPUT': _inLayer, 'TARGET_CRS': _stringWgs84Crs, 'OUTPUT': 'memory:temp'}
+        _parameter = {'INPUT': _in_layer, 'TARGET_CRS': _stringWgs84Crs, 'OUTPUT': 'memory:temp'}
         _wgs84Layer = processing.run('native:reprojectlayer', _parameter, context=_context, feedback=_feedback)['OUTPUT'] 
         return _wgs84Layer
                      
     def processAlgorithm(self, parameters, context, feedback):
-        inLayer = self.parameterAsLayer(parameters, self.INPUT, context)        
+        in_layer = self.parameterAsLayer(parameters, self.INPUT, context)
                         
         # -Reproject to WGS 84- 
-        wgs84Layer  = self.ReprojectToWgs84(inLayer, context, feedback)
+        wgs84_layer = self.reproject_to_wgs84(in_layer, context, feedback)
         feedback.pushInfo("Layer reprojected to 'WGS 84'") 
 
         feedback.setProgressText(self.tr('Creating single parts…'))
-        singlepartsLayer = processing.run("native:multiparttosingleparts", {'INPUT': inLayer, 'OUTPUT': 'memory:'}, feedback=feedback, context=context)['OUTPUT']
-                
+        singleparts_layer = processing.run("native:multiparttosingleparts", {'INPUT': in_layer, 'OUTPUT': 'memory:'}, feedback=feedback, context=context)['OUTPUT']
 
-        (sink, dest_id) = self.parameterAsSink(parameters, self.OUTPUT, context, wgs84Layer.fields(), QgsWkbTypes.Polygon, wgs84Layer.crs())      
-        sink.addFeatures(singlepartsLayer.getFeatures(), QgsFeatureSink.FastInsert) 
+        (sink, dest_id) = self.parameterAsSink(parameters, self.OUTPUT, context, wgs84_layer.fields(), QgsWkbTypes.Polygon, wgs84_layer.crs())
+        sink.addFeatures(singleparts_layer.getFeatures(), QgsFeatureSink.FastInsert)
         result = {self.OUTPUT: dest_id}
        
         return result
@@ -76,13 +76,13 @@ class reprojectandfinish(QgsProcessingAlgorithm):
         return 'fpboundarytools'
 
     def createInstance(self):
-        return reprojectandfinish()
+        return ReprojectAndFinish()
 
-    #def icon(self):
-    #    return QgsApplication.getThemeIcon("/algorithms/mAlgorithmExtractLayerExtent.svg")
+    # def icon(self):
+    #     return QgsApplication.getThemeIcon("/algorithms/mAlgorithmExtractLayerExtent.svg")
 
-    #def svgIconPath(self):
-    #    return QgsApplication.iconPath("/algorithms/mAlgorithmExtractLayerExtent.svg")
+    # def svgIconPath(self):
+    #     return QgsApplication.iconPath("/algorithms/mAlgorithmExtractLayerExtent.svg")
 
-    #def tags(self):
-    #    return 'polygon,vector,raster,extent,envelope,bounds,bounding,boundary,layer,round,rounded'.split(',')    
+    # def tags(self):
+    #     return 'polygon,vector,raster,extent,envelope,bounds,bounding,boundary,layer,round,rounded'.split(',')
